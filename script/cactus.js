649c6cccbe3ac6bee475263e4eabc1ce57dbe450
Thi Vieira <thiiagoviieira@gmail.com>
2021-01-13 00:33:23 -0300

    Funcoes adicionadas: Timer, botao play/pause, reset do game, pontuação, dark theme, fim de jogo

--- script/cactus.js
+++ script/cactus.js
@@ -2,35 +2,12 @@ const background = document.querySelector('.background');
 // import { position } from "./dino.js";
 let resultado;
 
-// início do jogo
-var tempo = 0;
-
-// referência ao timer
-var tempo_controle = 0;
-
-// está parado (=1) ou não (=0)
-var parado = 1;
-
-function atualiza_tempo()
-{
-    if (parado)
-        return;
-    var tempo_segundos = Math.floor((+new Date() - tempo) / 1000);
-    var display = tempo_segundos.toString() + " seg";
-    document.getElementById("display_tempo").innerHTML = display;
-    return;
-}
 
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
     let randomTime = Math.random() * 6000;
-
-    tempo = +new Date();
-    parado = 0;
-    atualiza_tempo();
-    tempo_controle = window.setInterval(atualiza_tempo, 600);
-
+    
     console.log(randomTime);
 
     cactus.classList.add('cactus');
@@ -44,14 +21,17 @@ function createCactus() {
         }
         else if (cactusPosition < 60 && cactusPosition > 0 && position < 60) {
             //Gamer Over
+            // debugger;
             clearInterval(leftInterval);
-            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
+            parado = 1;
+            window.clearInterval(tempo_controle);
             resultado = window.prompt('Você perdeu! Deseja jogar novamente ? <S/N>');
-            if (resultado === 'S') {
+            if (resultado === 'S' || resultado === 's') {
                 window.location.reload();
             }
             else {
-                //exibirPlacar();
+                document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
+                return;
             }
         }
         else {
@@ -62,5 +42,6 @@ function createCactus() {
 
     setTimeout(createCactus, randomTime);
 }
+
 
 // createCactus();
---
+++ script/timer.js
@@ -0,0 +1,104 @@
+(function () {
+    var delay = 3000;
+
+    var label = document.getElementById("#label");
+    var value = parseInt(label.textContent);
+
+    var progress = document.getElementById("#progress");
+
+    var resumeBtn = document.getElementById("#resume");
+    var pauseBtn = document.getElementById("#pause");
+    var resetBtn = document.getElementById("#reset");
+
+    var t = new timer(function () {
+        label.textContent = ++value;
+    }, delay);
+
+    progress.style.animationDuration = delay + "ms";
+
+    resumeBtn.addEventListener("click", function () {
+        t.resume();
+
+        pauseBtn.classList.remove("disabled");
+        resumeBtn.classList.add("disabled");
+        resetBtn.classList.add("disabled");
+
+        progress.classList.add("animate");
+        progress.classList.remove("pause");
+    });
+
+    pauseBtn.addEventListener("click", function () {
+        t.pause();
+
+        pauseBtn.classList.add("disabled");
+        resumeBtn.classList.remove("disabled");
+        resetBtn.classList.remove("disabled");
+
+        progress.classList.add("pause");
+    });
+
+    resetBtn.addEventListener("click", function () {
+        t.reset();
+
+        pauseBtn.classList.add("disabled");
+        resumeBtn.classList.remove("disabled");
+        resetBtn.classList.add("disabled");
+
+        // https://css-tricks.com/restart-css-animation/
+        progress.classList.remove("animate");
+        void progress.offsetWidth;
+        progress.classList.add("animate");
+    });
+}) ();
+
+function timer(callback, delay) {
+    var timerId;
+    var start;
+    var remaining = delay;
+
+    this.pause = function () {
+        window.clearTimeout(timerId);
+        remaining -= new Date() - start;
+    };
+
+    var resume = function () {
+        start = new Date();
+        timerId = window.setTimeout(function () {
+            remaining = delay;
+            resume();
+            callback();
+        }, remaining);
+    };
+    this.resume = resume;
+
+    this.reset = function () {
+        remaining = delay;
+    };
+}
+
+
+// início do jogo
+var tempo = 0;
+
+// referência ao timer
+var tempo_controle = 0;
+
+// está parado (=1) ou não (=0)
+var parado = 1;
+
+// tempo para alterar o fundo
+
+function atualiza_tempo() {
+    if (parado)
+        return;
+    var tempo_segundos = Math.floor((+new Date() - tempo) / 1000);
+    var display = "Distância: " + tempo_segundos.toString() * 100 + "km";
+    document.getElementById("display_time").innerHTML = display;
+
+    if (tempo_segundos >= 3) {
+        document.querySelector('body').style.background = "black";
+        document.querySelector('#display_time').style.color = "white";
+        document.querySelector('.background').style.background = "url(background_dark.png)";
+    }
+    return;
+}
